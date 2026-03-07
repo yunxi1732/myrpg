1b3480b36bf6743a7d92119f7669df0a75074401
yun707 <yun707txh@163.com>
2026-03-07 15:37:52 +0800

    技能系统初始化

--- Assets/Scripts/Enemy/Skeleton/SkeletonBattleState.cs
+++ Assets/Scripts/Enemy/Skeleton/SkeletonBattleState.cs
@@ -16,7 +16,7 @@ public class SkeletonBattleState : SkeletonGroundedState
     public override void Enter()
     {
         base.Enter();
-        player = GameObject.Find("Player").transform;
+        player = PlayerManager.instance.player.transform;
     }
 
     public override void Update()
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -17,8 +17,6 @@ public class Player : Entity
     [Header("Dash Info")]
     public float dashSpeed = 40f;
     public float dashDuraiton = 0.1f;
-    public float dashCooldown = 1.5f;
-    private float dashUsageTimer;
     public float dashDir { get; private set; }
 
     #region State
@@ -67,12 +65,13 @@ public class Player : Entity
 
     public void CheckForDashInput()
     {
-        dashUsageTimer -= Time.deltaTime;
+        if (IsWallDetected()) return;
+        
         dashDir = Input.GetAxisRaw("Horizontal");
         if (dashDir == 0) dashDir = facingDir;
-        if (Input.GetKeyDown(KeyCode.LeftShift) && dashUsageTimer < 0)
+        if (Input.GetKeyDown(KeyCode.LeftShift) && SkillManager.instance.dash.CanUseSkill())
         {
-            dashUsageTimer = dashCooldown;
+            
             stateMachine.ChangeState(dashState);
         }
     }
