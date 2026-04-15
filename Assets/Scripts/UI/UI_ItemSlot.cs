d752030baea291d799dab9ad46afd315b3ed94a8
yun707 <yun707txh@163.com>
2026-04-15 20:50:01 +0800

    equipment item finished

---
+++ Assets/Scripts/UI/UI_ItemSlot.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UI_ItemSlot : MonoBehaviour
+{
+    [SerializeField] private Image itemImage;
+    [SerializeField] private TextMeshProUGUI itemText;
+
+    public InventoryItem item;
+
+    public void UpdateSlot(InventoryItem _item)
+    {
+        item = _item;
+        itemImage.color = Color.white;
+
+        if (item != null)
+        {
+            itemImage.sprite = item.data.icon;
+
+            if (item.stackSize > 0)
+            {
+                itemText.text = item.stackSize.ToString();
+            }
+            else
+                itemText.text = "";
+        }
+    }
+}
