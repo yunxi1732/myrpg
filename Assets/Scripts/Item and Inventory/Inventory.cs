194933ae8648fd516110951f024650251bdde273
yun707 <yun707txh@163.com>
2026-05-28 22:15:25 +0800

    127

--- Assets/Scripts/Item and Inventory/Inventory.cs
+++ Assets/Scripts/Item and Inventory/Inventory.cs
@@ -141,7 +141,7 @@ public class Inventory : MonoBehaviour
 
     public void AddItem(ItemData _item)
     {
-        if (_item.itemType == ItemType.Equipment)
+        if (_item.itemType == ItemType.Equipment && CanAddItem())
             AddToInventory(_item);
         else if (_item.itemType == ItemType.Material)
             AddToStash(_item);
@@ -202,6 +202,16 @@ public class Inventory : MonoBehaviour
         }
 
         UpdateSlotUI();
+    }
+
+    public bool CanAddItem()
+    {
+        if (inventory.Count >= inventoryItemSlot.Length)
+        {
+            Debug.Log("no more space");
+            return false;
+        }
+        return true;
     }
 
     public bool canCraft(ItemData_Equipment _itemToCraft, List<InventoryItem> _requiredMaterials)
